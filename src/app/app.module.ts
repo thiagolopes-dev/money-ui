3790eecb2ffa400e7c50b1b526a18b5c5a387681
Thiago Lopes <thiagoolopezz@gmail.com>
2022-08-30 19:27:07 -0300

    integração primeng e primeflex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { PrimengModule } from './primeng.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    PrimengModule
   ],
   providers: [],
   bootstrap: [AppComponent]
