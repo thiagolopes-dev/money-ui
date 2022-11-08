ddf3e6050106bc019dc119fd561311c9e27dfcd8
Thiago Lopes <thiagoolopezz@gmail.com>
2022-11-08 20:42:33 -0300

    Add platform Android

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.moneyui.app',
+  appName: 'Money App',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
